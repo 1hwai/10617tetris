990f408c30784a1bbba0a071a8409c6c7b3a6f87
10617 조하원 <73703748+hawon10617@users.noreply.github.com>
2021-11-26 12:50:45 +0900

    Update tetris_rule.c

--- 10617tetris/tetris_rule.c
+++ 10617tetris/tetris_rule.c
@@ -26,7 +26,8 @@ int colmove = 0;
 
 void moves(struct Board* board, struct Piece* p, struct Piece* piece, int* built) {
 	char c;
-	const int KEY[2] = { 15, 20 + LINE - 1 };
+	static int cnt = 0;
+	const int KEY[2] = { 15 + COL, 20 + ROW - 1 };
 	//colmove
 	//x축 움직임 감지 시 1, 아니면 0
 	//벽에 붙는 거 방지용
@@ -37,42 +38,42 @@ void moves(struct Board* board, struct Piece* p, struct Piece* piece, int* built
 		if (c == -32) {
 			c = _getch();
 			switch (c) {
-			case LEFT:
-				draw(KEY[0], KEY[1], "LEFT");
-				p->x--;
-				if (valid(board, p, 0)) {
-					piece->x--;
-				}
-				colmove = 1;
-				break;
-			case RIGHT:
-				draw(KEY[0], KEY[1], "RIGHT");
-				p->x++;
-				if (valid(board, p, 0)) {
-					piece->x++;
-				}
-				colmove = 1;
-				break;
-			case UP:
-				draw(KEY[0], KEY[1], "UP");
-				rotate(p);
-				if (valid(board, p, 0)) {
-					for (int i = 0; i < piece->size; i++) {
-						for (int j = 0; j < piece->size; j++) {
-							piece->shape[i][j] = p->shape[i][j];
+				case LEFT:
+					draw(KEY[0], KEY[1], "LEFT");
+					p->x--;
+					if (valid(board, p, 0)) {
+						piece->x--;
+					}
+					colmove = 1;
+					break;
+				case RIGHT:
+					draw(KEY[0], KEY[1], "RIGHT");
+					p->x++;
+					if (valid(board, p, 0)) {
+						piece->x++;
+					}
+					colmove = 1;
+					break;
+				case UP:
+					draw(KEY[0], KEY[1], "UP");
+					rotate(p);
+					if (valid(board, p, 0)) {
+						for (int i = 0; i < piece->size; i++) {
+							for (int j = 0; j < piece->size; j++) {
+								piece->shape[i][j] = p->shape[i][j];
+							}
 						}
 					}
-				}
-				colmove = 1;
-				break;
-			case DOWN:
-				draw(KEY[0], KEY[1], "DOWN");
-				p->y++;
-				if (valid(board, p, 0)) {
-					piece->y++;
-					board->score++;
-				}
-				break;
+					colmove = 1;
+					break;
+				case DOWN:
+					draw(KEY[0], KEY[1], "DOWN");
+					p->y++;
+					if (valid(board, p, 0)) {
+						piece->y++;
+						board->score++;
+					}
+					break;
 			}
 		}
 		else if (c == SPACE) {
@@ -83,6 +84,7 @@ void moves(struct Board* board, struct Piece* p, struct Piece* piece, int* built
 				board->score+=2;
 			}
 			piece->y--;
+			cnt = 40; //하드드랍으로 바로 끝냄
 			//p->y--;
 		}
 		if (c == ESC) {
@@ -95,8 +97,7 @@ void moves(struct Board* board, struct Piece* p, struct Piece* piece, int* built
 				}
 			}
 		}
-		static int cnt = 0;
-		if (colmove == 0 && cnt == 40) {
+		if (colmove == 0 && cnt >= 40) {
 			p->y++;
 			if (!(valid(board, p, 1))) { //더 이상 움직이는 게 불가능하다면
 				for (int i = 0; i < piece->size; i++) {
@@ -263,6 +264,7 @@ void spawn(struct Piece* piece, int queue[2]) {
 		piece->shape[1][2] = 2;
 		piece->shape[1][3] = 2;
 		piece->size = 4;
+		piece->color = SKYBLUE;
 		break;
 		//0 1 2 3 4
 		//ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
@@ -278,6 +280,7 @@ void spawn(struct Piece* piece, int queue[2]) {
 		piece->shape[1][0] = 2;
 		piece->shape[1][1] = 2;
 		piece->size = 2;
+		piece->color = YELLOW;
 		break;
 		//0 1 2 3 4
 		//ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
@@ -290,6 +293,7 @@ void spawn(struct Piece* piece, int queue[2]) {
 		piece->shape[1][1] = 2;
 		piece->shape[1][2] = 2;
 		piece->size = 3;
+		piece->color = BLUE;
 		break;
 	case 4: //L
 		piece->shape[0][2] = 2;
@@ -297,6 +301,7 @@ void spawn(struct Piece* piece, int queue[2]) {
 		piece->shape[1][1] = 2;
 		piece->shape[1][2] = 2;
 		piece->size = 3;
+		piece->color = ORANGE;
 		break;
 	case 5: //S
 		piece->shape[0][1] = 2;
@@ -304,6 +309,7 @@ void spawn(struct Piece* piece, int queue[2]) {
 		piece->shape[1][0] = 2;
 		piece->shape[1][1] = 2;
 		piece->size = 3;
+		piece->color = GREEN;
 		break;
 	case 6: //2
 		piece->shape[0][0] = 2;
@@ -311,6 +317,7 @@ void spawn(struct Piece* piece, int queue[2]) {
 		piece->shape[1][1] = 2;
 		piece->shape[1][2] = 2;
 		piece->size = 3;
+		piece->color = RED;
 		break;
 	case 7: //T
 		piece->shape[0][1] = 2;
@@ -318,6 +325,7 @@ void spawn(struct Piece* piece, int queue[2]) {
 		piece->shape[1][1] = 2;
 		piece->shape[1][2] = 2;
 		piece->size = 3;
+		piece->color = PURPLE;
 		break;
 	}
 	queue[1] = random(1, 7);
